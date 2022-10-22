0a010e0fc4f125ac8d8d71f166673cb758c4858d
PC <krasimir.bozhilov@gmail.com>
2022-10-22 20:25:17 +0300

    feat: create database.py

---
+++ server/data/database.py
@@ -0,0 +1,43 @@
+from mariadb import connect
+from mariadb.connections import Connection
+
+
+def _get_connection() -> Connection:
+    return connect(
+        user='root',
+        password='root',
+        host='localhost',
+        port=3306,
+        database='job_match_db'
+    )
+
+
+def read_query(sql: str, sql_params=()):
+    with _get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, sql_params)
+
+        return list(cursor) #cursor is a tuple, so a list of tuples
+
+def read_query_single_element(sql: str, sql_params=()):
+    with _get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, sql_params)
+
+        return cursor.fetchone()
+
+def insert_query(sql: str, sql_params=()) -> int:
+    with _get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, sql_params)
+        conn.commit()
+
+        return cursor.lastrowid
+
+def update_query(sql: str, sql_params=()) -> bool:
+    with _get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql, sql_params)
+        conn.commit()
+
+        return cursor.rowcount
