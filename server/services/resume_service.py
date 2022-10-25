dd724205dd3a1b602f59728207bd5a5e745f8666
PC <krasimir.bozhilov@gmail.com>
2022-10-25 19:17:44 +0300

    fix:bug fix get_all_skills_resume_by_id

--- server/services/resume_service.py
+++ server/services/resume_service.py
@@ -1,5 +1,5 @@
 from server.data.database import read_query
-from server.data.models import Resume, Status
+from server.data.models import Resume, Status, Skill
 
 
 def get_all_active_resumes_by_professional_id(professional_id: int):
@@ -25,12 +25,13 @@ def get_all_archived_resumes_by_professional_id(professional_id: int):
         return [0]
 
 
-def get_all_skills_resume_by_id(id: int):
+def get_all_skills_resume_by_id(resume_id: int):
     data = read_query(
-        '''SELECT s.id,s.name,r_s.stars
-                 FROM skills as s
-                 LEFT JOIN
-                resume_skils as r_s
-                ON s.id=r_s.resume_id
-                    WHERE s.'''
-    )
+        '''SELECT s.name,r_s.stars
+                 FROM skills as s 
+                 RIGHT JOIN 
+                resumes_skills as r_s
+                ON s.id=r_s.skill_id
+                    WHERE r_s.resume_id=?''', (resume_id,))
+    return (Skill(name=name, stars=stars)
+            for name, stars in data)
