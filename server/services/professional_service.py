0348ea66cf081225ea7f0124846b48da8039081c
Krasimir Bozhilov <krasimir.bozhilov@gmail.com>
2022-10-25 14:59:30 +0300

    feat: new all method

---
+++ server/services/professional_service.py
@@ -0,0 +1,25 @@
+from server.data.database import read_query
+from server.data.models import Professional
+
+
+def all(search: str = None):
+    if search is None:
+        data = read_query(
+            '''SELECT p.id, p.user_name, p.password,p.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
+            c.email,c.phone,c.address,c.town_id
+                FROM professionals as p
+                LEFT JOIN
+                contacts as c
+                ON p.contact_id=c.id''')
+
+    else:
+        data = read_query(
+            '''SELECT p.id, p.user_name, p.password,p.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
+            c.email,c.phone,c.address,c.town_id
+                FROM professionals as p
+                LEFT JOIN
+                contacts as c
+                ON p.contact_id=c.id
+               WHERE p.first_name LIKE ?''', (f'%{search}%',))
+
+    return (Professional.from_query_result(*row) for row in data)
