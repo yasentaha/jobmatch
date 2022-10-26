35ed416f17f773d173891246dce29fcf711e5b6d
PC <krasimir.bozhilov@gmail.com>
2022-10-26 19:26:01 +0300

    fix: bug fix all and get_by_id query

--- server/services/professional_service.py
+++ server/services/professional_service.py
@@ -5,21 +5,27 @@ from server.data.models import Professional, Resume, Status
 def all(search: str = None):
     if search is None:
         data = read_query(
-            '''SELECT p.id, p.user_name, p.password,p.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
-            c.email,c.phone,c.address,c.town_id
-                FROM professionals as p
-                LEFT JOIN
-                contacts as c
-                ON p.contact_id=c.id''')
+            '''SELECT u.id, u.user_name, u.password,u.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
+            c.email,c.phone,c.address,t.id
+                FROM users as u
+                LEFT JOIN professionals as p 
+                ON u.id= p.user_id
+                LEFT JOIN contacts as c
+                ON p.contact_id=c.id
+                LEFT JOIN towns as t
+                ON c.town_id=t.id''')
 
     else:
         data = read_query(
-            '''SELECT p.id, p.user_name, p.password,p.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
-            c.email,c.phone,c.address,c.town_id
-                FROM professionals as p
-                LEFT JOIN
-                contacts as c
+            '''SELECT u.id, u.user_name, u.password,u.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
+            c.email,c.phone,c.address,t.id
+                FROM users as u
+                LEFT JOIN professionals as p 
+                ON u.id= p.user_id
+                LEFT JOIN contacts as c
                 ON p.contact_id=c.id
+                LEFT JOIN towns as t
+                ON c.town_id=t.id
                WHERE p.first_name LIKE ?''', (f'%{search}%',))
 
     return (Professional.from_query_result(*row) for row in data)
@@ -27,14 +33,15 @@ def all(search: str = None):
 
 def get_by_id(id: int):
     data = read_query(
-        '''SELECT p.id, p.user_name, p.password,p.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
-        c.email,c.phone,c.address,c.town_id
-            FROM professionals as p
-            LEFT JOIN
-            contacts as c
-            ON p.contact_id=c.id
-            WHERE p.id=?''', (id,))
+        '''SELECT u.id, u.user_name, u.password,u.role,p.first_name,p.last_name,p.summary,p.busy,p.image_url,
+            c.email,c.phone,c.address,t.id
+                FROM users as u
+                LEFT JOIN professionals as p 
+                ON u.id= p.user_id
+                LEFT JOIN contacts as c
+                ON p.contact_id=c.id
+                LEFT JOIN towns as t
+                ON c.town_id=t.id
+            WHERE u.id=?''', (id,))
 
     return (Professional.from_query_result(*row) for row in data)
-
-
