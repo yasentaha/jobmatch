b0679d940ce4ca2f14fb1c07f5728fc019bf00d0
yasentaha <yasen.h.taha@gmail.com>
2022-10-27 17:53:12 +0300

    fix: change methods to comply with db update

--- server/routers/users.py
+++ server/routers/users.py
@@ -11,65 +11,55 @@ users_router = APIRouter(prefix='/users')
 @users_router.post('/professionals/register')
 def register_professional(data: ProfessionalRegisterData):
 
-    mandatory_fields_user_contact(data.user_name, data.password, data.confirm_password, data.email, data.address, data.town_name)
+    mandatory_fields_user_contact(data.user_name, data.password, data.confirm_password, data.email, data.town_name)
     
     mandatory_fields_professional(data.first_name, data.last_name)
+
+    role = 'professional'
 
     town_id = user_service.get_town_id_by_name(data.town_name)
     if not town_id:
         return NotFound(f'Town {data.town_name} is not a valid Bulgarian District Town name')
+    
+    if user_service.email_exists(data.email):
+        return BadRequest(f'Email address {data.email} is already used. Forgot password?')
 
-    user = user_service.create_user(data.user_name, data.password)
+    user = user_service.create_user(data.user_name,data.password,role,data.email,town_id,data.address,data.phone)
     if not user:
         return BadRequest(f'Username {data.user_name} is taken.')
 
-    contact = user_service.create_contact(data.email, data.address, town_id, data.phone)
-    if not contact:
-            return BadRequest(f'User with {data.email} already exists.')
-    
-    professional = user_service.create_professional(user.id, data.first_name, data.last_name, contact.id, data.summary, data.image_url)
+    professional_created = user_service.create_professional(user.id, data.first_name, data.last_name, data.summary)
 
-    return ProfessionalResponse(professional.id, 
-                                user.user_name, 
-                                professional.first_name,
-                                professional.last_name,
-                                professional.summary,
-                                professional.image_url,
-                                contact.email,
-                                contact.phone,
-                                contact.address,
-                                data.town_name)
+    if professional_created:
+
+        return Success(f'Professional account created for {data.first_name} {data.last_name}. Please log in to continue.')
+    #else think if the above may not be successful, if yes, delete the row in users (rollback)
 
 @users_router.post('/companies/register')
 def register_company(data: CompanyRegisterData):
 
-    mandatory_fields_user_contact(data.user_name, data.password, data.confirm_password, data.email, data.address, data.town_name)
+    mandatory_fields_user_contact(data.user_name, data.password, data.confirm_password, data.email, data.town_name)
     
-    mandatory_fields_company(data.company_name, data.description)
+    mandatory_fields_company(data.company_name, data.description, data.address)
+
+    role = 'company'
 
     town_id = user_service.get_town_id_by_name(data.town_name)
     if not town_id:
         return NotFound(f'Town {data.town_name} is not a valid Bulgarian District Town name')
 
-    user = user_service.create_user(data.user_name, data.password)
+    if user_service.email_exists(data.email):
+        return BadRequest(f'Email address {data.email} is already used. Forgot password?')
+
+    user = user_service.create_user(data.user_name,data.password,role,data.email,town_id,data.address,data.phone)
     if not user:
         return BadRequest(f'Username {data.user_name} is taken.')
-
-    contact = user_service.create_contact(data.email, data.address, town_id, data.phone)
-    if not contact:
-            return BadRequest(f'User with {data.email} already exists.')
     
-    company = user_service.create_company(user.id, data.company_name, data.description, contact.id, data.logo_url)
+    company_created = user_service.create_company(user.id, data.company_name, data.description)
 
-    return CompanyResponse(company.id,
-                            user.user_name,
-                            company.company_name,
-                            company.description,
-                            company.logo_url,
-                            contact.email,
-                            contact.phone,
-                            contact.address,
-                            data.town_name)
+    if company_created:
+
+        return Success(f'Company account created for {data.company_name}. Please log in to continue.')
 
 
 @users_router.post('/login')
@@ -78,13 +68,14 @@ def login(data: LoginData):
 
     if user:
         token = create_token(user)
-        return {'token': token}
+        return {'token': token,
+                'role': user.role}
     
     else:
         return BadRequest('Invalid login data')
 
 
-def mandatory_fields_user_contact(user_name:str, password, confirm_password:str, email:str, address:str, town_name:str):
+def mandatory_fields_user_contact(user_name:str, password, confirm_password:str, email:str, town_name:str):
     if not user_name:
         return BadRequest('User Name field is mandatory!')
     
@@ -105,9 +96,6 @@ def mandatory_fields_user_contact(user_name:str, password, confirm_password:str,
 
     if not user_service.valid_email(email):
         return BadRequest('Please enter a valid email address.')
-#ATTENTION DO WE NEED ADDRESS TO BE MANDATORY??
-    if not address:
-        return BadRequest('Please enter your address.')
     
     if not town_name:
         return BadRequest('Town Name field is mandatory!')
@@ -124,13 +112,15 @@ def mandatory_fields_professional(first_name:str, last_name: str):
     pass
 
     
-def mandatory_fields_company(company_name:str, description: str):
+def mandatory_fields_company(company_name:str, description: str, address:str):
     if not company_name:
         return BadRequest('Company Name field is mandatory!')
 
     if not description:
         return BadRequest('Description field is mandatory!')
-
+    
+    if not address:
+        return BadRequest('Please enter your address.')
     pass
 
     
