63a340d43d31b796ab973831fd5577ceb5b86f98
PC <krasimir.bozhilov@gmail.com>
2022-10-30 21:28:26 +0200

    fix: bug fix

--- server/services/resume_service.py
+++ server/services/resume_service.py
@@ -3,26 +3,26 @@ from server.data.models import Resume, Status, Skill
 
 
 def all_active_resumes_without_job_salary_and_description(id: int):
-
     data = read_query(
-                '''SELECT r.id, r.title, r.description, r.min_salary, r.max_salary, r.work_place, r.status, r.town_id,r.main 
-                        FROM resumes as r
-                        WHERE r.professional_id=? AND r.status=?''', (id, f'%{Status.ACTIVE}%'))
+        '''SELECT r.id, r.title, r.description, r.min_salary, r.max_salary, r.work_place, r.status, r.town_id,r.main 
+                FROM resumes as r
+                WHERE r.professional_id=? AND r.status=?''', (id, f'%{Status.ACTIVE}%'))
 
     return (Resume(id=id, title=title, description='None', min_salary=0, max_salary=0,
-            work_place=work_place, status=status, town_id=town_id, main=main)
-    for id, title, description, min_salary, max_salary, work_place, status, town_id, main in data)
+                   work_place=work_place, status=status, town_id=town_id, main=main)
+            for id, title, description, min_salary, max_salary, work_place, status, town_id, main in data)
+
 
 def all_hidden_resumes(id: int):
-
     data = read_query(
-                '''SELECT r.id, r.title, r.description, r.min_salary, r.max_salary, r.work_place, r.status, r.town_id,r.main 
-                        FROM resumes as r
-                        WHERE r.professional_id=? AND r.status=?''', (id, f'%{Status.HIDDEN}%'))
+        '''SELECT r.id, r.title, r.description, r.min_salary, r.max_salary, r.work_place, r.status, r.town_id,r.main 
+                FROM resumes as r
+                WHERE r.professional_id=? AND r.status=?''', (id, f'%{Status.HIDDEN}%'))
 
     return (Resume(id=id, title=title, description=description, min_salary=min_salary, max_salary=max_salary,
-            work_place=work_place, status=status, town_id=town_id, main=main)
-    for id, title, description, min_salary, max_salary, work_place, status, town_id, main in data)
+                   work_place=work_place, status=status, town_id=town_id, main=main)
+            for id, title, description, min_salary, max_salary, work_place, status, town_id, main in data)
+
 
 def create(resume: Resume, insert_data=None):
     if insert_data is None:
@@ -44,25 +44,27 @@ def get_resume_by_id(professional_id: int, resume_id: int):
                     FROM resumes as r
                     WHERE r.professional_id=? AND r.id=?''', (professional_id, resume_id))
 
-    return (Resume(id=id, title=title, description=description, min_salary=min_salary, max_salary=max_salary,
-                   work_place=work_place, status=status, town_id=town_id, main=main)
-            for id, title, description, min_salary, max_salary, work_place, status, town_id, main in data)
+    return Resume(id=data.id, title=data.title, description=data.description, min_salary=data.min_salary,
+                  max_salary=data.max_salary, work_place=data.work_place, status=data.status,
+                  town_id=data.town_id, main=data.main)
 
 
-# def edit_resume_by_id(professional_id: int, resume_id: int, title: str, description: str, min_salary: int,
-#                       max_salary: int, work_place: int, status: str, town_id: int, main: int, update_data=None):
-#     if update_data is None:
-#         update_data = update_query
-#
-#     update_data(
-#         '''UPDATE resumes
-#             SET title=?, description=?, min_salary=?, max_salary=?,work_place=?,status=?,town_id=?,main=?
-#              WHERE professional_id = ? AND resumes.id=?''',
-#         (title, description, min_salary, max_salary, work_place,
-#          status, town_id, main, professional_id, resume_id))
-#
-#     return (Resume(id=resume_id, title=title, description=description, min_salary=min_salary,
-#                    max_salary=max_salary, work_place=work_place, status=status, town_id=town_id, main=main))
+
+def edit_resume_by_professional_idand_resume_id(professional_id: int, resume_id: int, resume: Resume, update_data=None):
+    if update_data is None:
+        update_data = update_query
+
+    update_data(
+        '''UPDATE resumes
+            SET title=?, description=?, min_salary=?, max_salary=?,work_place=?,status=?,town_id=?,main=?
+             WHERE professional_id = ? AND resumes.id=?''',
+        (resume.title, resume.description, resume.min_salary, resume.max_salary, resume.work_place,
+         resume.status, resume.town_id, resume.main, professional_id, resume_id))
+
+    return Resume(id=update_data.id, title=update_data.title, description=update_data.description,
+                  min_salary=update_data.min_salary, max_salary=update_data.max_salary,
+                  work_place=update_data.work_place, status=update_data.status,
+                  town_id=update_data.town_id, main=update_data.main)
 
 
 def get_all_active_resumes_by_professional_id(professional_id: int):
