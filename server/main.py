5b9f0c77022d5d70cdea6bd27c68b6d25068b994
yasentaha <yasen.h.taha@gmail.com>
2022-11-02 16:50:47 +0200

    feat: together during teams meeting - add professional router

--- server/main.py
+++ server/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from server.routers.users import users_router
+from server.routers.professionals import professionals_router
 
 app = FastAPI()
 app.include_router(users_router)
+app.include_router(professionals_router)
