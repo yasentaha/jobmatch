b0370a4adc02c52dcdc82b27b90c94c227780074
DimitarVasilev <dimityr931@gmail.com>
2022-11-02 23:03:45 +0200

    feat: add companies_router

--- server/main.py
+++ server/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from server.routers.users import users_router
 from server.routers.professionals import professionals_router
+from server.routers.companies import companies_router
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(professionals_router)
+app.include_router(companies_router)
