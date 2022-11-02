feb1dc40e1a0a45afdf1841e22f9b072d28a7da0
DimitarVasilev <dimityr931@gmail.com>
2022-11-02 23:00:15 +0200

    feat: added new endpoints in companies

--- server/routers/companies.py
+++ server/routers/companies.py
@@ -2,24 +2,71 @@ from fastapi import APIRouter, Header
 from pydantic import BaseModel
 
 from server.common.auth import get_user_or_raise_401
-from server.common.responses import NotFound, Forbidden, Unauthorized
-from server.data.models import Company, JobAd, CompanyInfo
-from server.services.company_service import get_company_info_by_id, get_company_by_id, get_all_companies, get_number_of_all_active_job_ads_by_company_id
+from server.common.responses import NotFound, Forbidden, Unauthorized, BadRequest, Success
+from server.data.models import Company, JobAd, CompanyInfo, CompanyResponseModel, PersonalCompanyResponseModel
+from server.services.company_service import get_company_info_by_id, get_company_by_id, edit_company, get_all_companies
+from server.services.user_service import edit_user_info, get_town_id_by_name
 from server.services import company_service, job_ad_service
 
 
 companies_router = APIRouter(prefix='/companies')
 
 @companies_router.get('/')
-def get_companies(sort: str | None = None, x_token=Header()):
+def get_companies(search: str | None = None, search_by: str | None = None, sort: str | None = None, x_token=Header()):
     user = get_user_or_raise_401(x_token)
 
     if user:
-        companies = company_service.get_all_companies()
+        companies = company_service.get_all_companies(search, search_by)
     else:
-        return Forbidden('Please Login!')
+        return Forbidden('Please log in!')
 
     if sort and (sort == 'asc' or sort == 'desc'):
         return company_service.sort(companies, reverse=sort == 'desc')
     else:
         return companies
+
+@companies_router.get('/{id}')
+def get_company_by_Id(id: int, x_token: str = Header()):
+    user = get_user_or_raise_401(x_token)
+
+    company = company_service.get_company_by_id(id)
+
+    if company is None:
+        return NotFound(f'Company with ID: {id} not found!')
+
+    if user.id == id:
+        return PersonalCompanyResponseModel(
+            company= company,
+            active_job_ads=job_ad_service.get_number_of_all_active_job_ads_by_company_id(id),
+            list_of_matches=job_ad_service.get_list_of_matches(id)
+            )
+
+    else:
+        return CompanyResponseModel(
+            company= company,
+            active_job_ads=job_ad_service.get_number_of_all_active_job_ads_by_company_id(id))
+
+@companies_router.put('/{id}')
+def edit_company_by_id(id: int, company: Company, x_token: str = Header()):
+    user = get_user_or_raise_401(x_token)
+
+    if not user.id == id:
+        return Unauthorized('You do not have permission to change Company info!')
+
+    town_id = get_town_id_by_name(company.town_name)
+    if not town_id:
+        return NotFound(f'Town {company.town_name} is not a valid Bulgarian District Town name')
+    
+    error_msg = 'Your query is incorrect, please review your new info.'
+
+    edited_user_info = edit_user_info(id, company.email, company.phone, company.address, town_id)
+    
+    if edited_user_info:
+        edited_company = edit_company(id, company)
+    else:
+        return BadRequest(error_msg)
+    
+    if edited_company:
+        return Success(f'Successfully updated info for {company.company_name} company!')
+    else:
+        return BadRequest(error_msg)
