8439deb7670d0d28569cac08b25fa7b0f14f4190
Krasimir Bozhilov <krasimir.bozhilov@gmail.com>
2022-11-04 10:45:08 +0200

    feat:add include_router in main

--- server/main.py
+++ server/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from server.routers.users import users_router
 from server.routers.professionals import professionals_router
 from server.routers.companies import companies_router
+from server.routers.resumes import resumes_router
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(professionals_router)
 app.include_router(companies_router)
+app.include_router(resumes_router)
