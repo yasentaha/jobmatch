6110ffb18b66d6447342eff973e125380f1058a3
DimitarVasilev <dimityr931@gmail.com>
2022-11-04 22:11:26 +0200

    feat: adding new functionality in job_ads_router

--- server/routers/job_ads.py
+++ server/routers/job_ads.py
@@ -2,8 +2,31 @@ from fastapi import APIRouter, Header
 from pydantic import BaseModel
 
 from server.common.auth import get_user_or_raise_401
-from server.common.responses import NotFound, Forbidden, Unauthorized
-from server.data.models import Company, JobAd
+from server.common.responses import NotFound, Forbidden, Unauthorized, Success
+from server.data.models import Company, JobAd, CreateJobAd, Role
 from server.services import company_service, job_ad_service
+from server.services.job_ad_service import add_skills, return_skills_with_ids, add_skill_to_job_ad
 
 job_ads_router = APIRouter(prefix='/job_ads')
+
+@job_ads_router.post('/')
+def create_resume(create_job_ad: CreateJobAd, x_token= Header()):
+    user = get_user_or_raise_401(x_token)
+
+    if not user:
+        return Unauthorized('Access denied, you do not have permission to access on this server!')
+
+    if user.role != Role.COMPANY:
+        return Forbidden('You do not have permission to create a job_ad!')
+
+
+    if create_job_ad.skill_requirements:
+        add_skills(create_job_ad.skill_requirements)
+
+    skills_with_ids = return_skills_with_ids(create_job_ad.skill_requirements)
+
+    new_job_ad = job_ad_service.create_job_ad(user.id, create_job_ad)
+    if new_job_ad:
+        [add_skill_to_job_ad(new_job_ad.id, skill) for skill in skills_with_ids]
+
+    return Success(f'Resume with title {new_job_ad.title} was created!')
