1dd7f72ab4a6fb145668192fc1978e989bb03c03
DimitarVasilev <dimityr931@gmail.com>
2022-11-06 20:03:11 +0200

    feat: adding new functionality in job_ads routers

--- server/routers/job_ads.py
+++ server/routers/job_ads.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel
 
 from server.common.auth import get_user_or_raise_401
 from server.common.responses import NotFound, Forbidden, Unauthorized, Success
-from server.data.models import Company, JobAd, CreateJobAd, Role
+from server.data.models import Company, JobAd, CreateJobAd, Role, JobAdResponseModel
 from server.services import company_service, job_ad_service
-from server.services.job_ad_service import add_skills, return_skills_with_ids, add_skill_to_job_ad
+from server.services.job_ad_service import add_skills, return_skills_with_ids, add_skill_to_job_ad, get_job_ad_by_id, get_all_skills_for_job_ad_id
 
 job_ads_router = APIRouter(prefix='/job_ads')
 
@@ -30,3 +30,15 @@ def create_resume(create_job_ad: CreateJobAd, x_token= Header()):
         [add_skill_to_job_ad(new_job_ad.id, skill) for skill in skills_with_ids]
 
     return Success(f'Resume with title {new_job_ad.title} was created!')
+
+@job_ads_router.get('/{id}')
+def get_job_ad(id: int, x_token= Header()):
+    user = get_user_or_raise_401(x_token)
+
+    if user:
+        return JobAdResponseModel(
+            resume=get_job_ad_by_id(id),
+            skills=get_all_skills_for_job_ad_id(id)
+        )
+
+    return Unauthorized('Please log in!')
