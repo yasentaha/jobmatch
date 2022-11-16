f42e903d07c12356c4c74c411c34da58495adee1
yasentaha <yasen.h.taha@gmail.com>
2022-11-16 18:21:54 +0200

    refactor: together during teams meeting

--- server/routers/resumes.py
+++ server/routers/resumes.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter, Header
-from pydantic import BaseModel
 from server.common.auth import get_user_or_raise_401
 from server.common.responses import Forbidden, Unauthorized, Success, BadRequest, NotFound
-from server.data.models import Resume, Skill, CreateResume, Role, ResumeResponseModel, ResumeWithSkillsResponseModel
-from server.services import resume_service, user_service, job_ad_service, match_request_service, professional_service, company_service
-from server.services.resume_service import create_resume_and_add_skill, get_resume_by_id, get_all_resume_skills_by_id, \
-    edit_resume_by_professional_id_and_resume_id, add_skills, return_skills_with_ids, add_skill_to_resume, \
-        main_resume_for_professional_exists, change_resume_main, validate_stars
+from server.data.models import Resume, Role, ResumeResponseModel, Status
+from server.services import resume_service, job_ad_service, match_request_service, professional_service, company_service
+from server.services.resume_service import get_resume_by_id, edit_resume_by_id, \
+                                            main_resume_for_professional_exists, change_resume_main
 from server.services.user_service import get_professional_fullname_by_id
-
+from server.common.validations_and_methods import add_skills, validate_stars, validate_work_place, validate_status
+from server.common.mailjet_service import send_match_request_email, send_its_a_match
 
 resumes_router = APIRouter(prefix='/resumes')
 
@@ -23,6 +22,11 @@ def create_resume(resume: Resume, x_token= Header()):
     if user.role != Role.PROFESSIONAL:
         return Forbidden('You do not have permission to create a resume!')
 
+    if not validate_status(resume.status, False):
+        return BadRequest('The given status is incorrect!')
+    
+    if not validate_work_place(resume.work_place):
+        return BadRequest('Invalid work place!')
 
     if not resume.skills:
         return BadRequest('You need to add at least one skill to your resume.')
@@ -43,14 +47,24 @@ def create_resume(resume: Resume, x_token= Header()):
 
 
 @resumes_router.put('/{resume_id}')
-def edit_resume(resume_id: int, resume: Resume, x_token= Header()):
+def edit_resume(resume_id: int, resume: Resume, x_token= Header(None)):
     user = get_user_or_raise_401(x_token)
+
+    current_resume = get_resume_by_id(resume_id)
+    if current_resume.status == Status.MATCHED:
+        return Forbidden('You cannot edit a Matched Resume!')
     
     if not user:
         return Unauthorized('Please log in!')
 
     if user.id != resume.professional_id:
         return Forbidden('You do not have permission to edit this resume!')
+
+    if not validate_status(resume.status, False):
+        return BadRequest('The given status is incorrect!')
+    
+    if not validate_work_place(resume.work_place):
+        return BadRequest('Invalid work place!')
 
     if not resume.skills:
         return BadRequest('You need to leave at least one skill to your resume.')
@@ -61,15 +75,15 @@ def edit_resume(resume_id: int, resume: Resume, x_token= Header()):
 
         add_skills(resume.skills)
 
+    edited_resume = edit_resume_by_id(resume_id, resume)
 
-    edited_resume = edit_resume_by_professional_id_and_resume_id(user.id, resume_id, resume)
     if edited_resume:
         return ResumeResponseModel(full_name=get_professional_fullname_by_id(edited_resume.professional_id), 
                 resume=edited_resume)
 
 
 @resumes_router.get('/{id}')
-def view_resume(id: int, x_token= Header()):
+def view_resume(id: int, x_token= Header(None)):
     user = get_user_or_raise_401(x_token)
 
     if user:
@@ -83,18 +97,18 @@ def view_resume(id: int, x_token= Header()):
     return Unauthorized('Please log in!')
 
 @resumes_router.get('/')
-def get_resumes(search: str | None = None, search_by: str | None = None, threshold: int | None = None,combined: bool | None = None,x_token=Header()):
+def get_resumes(search: str | None = None, search_by: str | None = None, threshold: int | None = None,combined: bool | None = None,x_token=Header(None)):
     user = get_user_or_raise_401(x_token)
+
+    if not user:
+        return Unauthorized('Please log in!')
 
     search_validation = ['salary_range', 'location', 'skills', 'job_ad']
 
     if search_by and search_by not in search_validation:
         return BadRequest(f'Cannot search by parameter {search_by}.')
 
-    if user:
-        resumes = resume_service.all_active(search, search_by, threshold, combined)
-    else:
-        return Forbidden('Please log in!')
+    resumes = resume_service.all_active(search, search_by, threshold, combined)
 
     if not resumes:
         return NotFound('No resumes match your search.')
@@ -105,20 +119,8 @@ def get_resumes(search: str | None = None, search_by: str | None = None, thresho
     return resumes_response
 
 
-@resumes_router.get('/{id}/resumes/hidden')
-def get_resumes(professional_id: int, x_token=Header()):
-    user = get_user_or_raise_401(x_token)
-
-    if user.id == id or user.is_admin():
-        resumes = resume_service.all_hidden_resumes_by_id(professional_id)
-    else:
-        return Unauthorized('Access denied, you do not have permission to access on this server!')
-
-    return resumes
-
-
 @resumes_router.post('/{id}')
-def send_match_request(id: int, job_ad_id:int, x_token= Header()):
+def send_match_request(id: int, job_ad_id:int, x_token= Header(None)):
 
     user = get_user_or_raise_401(x_token)
 
@@ -142,6 +144,7 @@ def send_match_request(id: int, job_ad_id:int, x_token= Header()):
         return Forbidden('You do not own this Job Ad!')
 
     professional = professional_service.get_professional_by_id(resume.professional_id)
+    professional_full_name = f'{professional.first_name} {professional.last_name}'
 
     company = company_service.get_company_by_id(user.id)
 
@@ -151,26 +154,23 @@ def send_match_request(id: int, job_ad_id:int, x_token= Header()):
         if existing_match_request.requestor_id == user.id:
             return BadRequest(f'You already sent a match request with Job Ad with ID {job_ad_id} to Resume with ID {id}!')
         elif existing_match_request.requestor_id == resume.professional_id:
-            #update match request with match = 1 ???
             match_request_service.its_a_match(existing_match_request.id)
             
-            #update professional to busy
             professional_service.make_professional_busy(professional.id)
 
-            #update company's successful matches +1
             company_service.update_successful_matches(company.id)
 
-            #Resume status -> Matched
             resume_service.make_resume_matched(resume.id)
 
-            #JobAd status -> Archived
             job_ad_service.make_job_ad_archived(job_ad.id)
 
-            #send email to Company and to Professional
+            send_its_a_match(professional.email, professional_full_name, resume.title, company.company_name,
+                            job_ad.title, False)
+            #SEND TO COMPANY AS WELL
             return Success(f'Instant Match!')
     else:
         match_request_service.initiate_match_request(user.id, id, job_ad_id)
-        #send email to Professional
-        return Success(f'Your Match Request was successfully sent to {professional.first_name} {professional.last_name}!')
+        send_match_request_email(professional.email, professional_full_name,resume.title, company.company_name, job_ad, False)
+        return Success(f'Your Match Request was successfully sent to {professional_full_name}!')
 
     
