78535cf106484c435eefac6e7c29a23eccbc5c85
yasentaha <yasen.h.taha@gmail.com>
2022-11-16 18:22:12 +0200

    refactor: together during teams meeting

--- server/services/company_service.py
+++ server/services/company_service.py
@@ -1,6 +1,6 @@
-from server.data.database import read_query, update_query, insert_query, read_query_single_element
-from server.data.models import Company, CompanyInfo, Role
-from mariadb import DataError, OperationalError
+from server.data.database import read_query, update_query, read_query_single_element
+from server.data.models import Company, Role
+from mariadb import DataError
 
 
 
@@ -61,52 +61,30 @@ def get_all_companies(search: str | None = None, search_by: str | None = None):
     return (Company.from_query_result(*row) for row in data)
 
 
-def get_company_info_by_id(id: int):
-    data = read_query(
-        '''SELECT user_id, c.company_name,c.description,c.successful_matches
-        FROM 
-            companies AS c 
-        WHERE c.users_id=?''', (id,))
-
-    return next((CompanyInfo.from_query_result(*row) for row in data), None)
-
-
-# def edit_company_info(id:int,company_info: CompanyInfo,update_data=None):
-#     if update_data is None:
-#         update_data = update_query
-#     try:
-#         update_data(
-#             '''UPDATE companies
-#                 SET company_name = ? ,description = ?, successful_matches = ?
-#                 WHERE user_id = ?''', (company_info.company_name, company_info.description,
-#                                         company_info.successful_matches,id))
-
-#         return True
-    
-#     except DataError:
-#         return False
-
-def edit_company(id:int,company_info: CompanyInfo,update_data=None):
+def edit_company(id:int, company:Company, update_data=None):
     if update_data is None:
         update_data = update_query
     try:
         update_data(
         '''UPDATE companies
                 SET company_name = ? ,description = ?, successful_matches = ?
-                WHERE user_id = ?''', (company_info.company_name, company_info.description,
-                                        company_info.successful_matches,id))
+                WHERE user_id = ?''', (company.company_name, company.description,
+                                        company.successful_matches,id))
 
         return True
     
     except DataError:
         return False
 
+
 def update_successful_matches(id: int):
-    current_company_matches = (read_query_single_element('SELECT successful_matches from companies where id=?', (id,)))[0]
+    current_company_matches = (read_query_single_element('SELECT successful_matches from companies where user_id=?', (id,)))[0]
 
     updated_company_matches = current_company_matches + 1
 
-    update_query('UPDATE companies SET successful_matches=? WHERE id=?', (updated_company_matches, id))
+    update_query('UPDATE companies SET successful_matches=? WHERE user_id=?', (updated_company_matches, id))
+
+
 
 def sort(companies: list[Company], *, attribute='name', reverse=False):
 
