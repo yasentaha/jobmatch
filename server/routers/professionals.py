9e1d127af0b8d998c5c707971ab2aae81342d501
yasentaha <yasen.h.taha@gmail.com>
2022-11-16 18:21:46 +0200

    refactor: together during teams meeting

--- server/routers/professionals.py
+++ server/routers/professionals.py
@@ -2,30 +2,20 @@ from fastapi import APIRouter, Header
 from pydantic import BaseModel
 from server.common.auth import get_user_or_raise_401
 from server.common.responses import NotFound, Forbidden, Unauthorized, Success, BadRequest
-from server.data.models import (Professional, Resume, ProfessionalInfo, ResumeWithSkillsResponseModel, Role, 
-                                ResumeWithoutDescriptionAndSalary, ResumeWithoutDescriptionAndSalaryResponse,
-                                ProfessionalMatchRequestResponse)
+from server.data.models import (Professional, ResumeWithSkillsResponseModel, Role, 
+                                ResumeWithoutDescriptionAndSalaryResponse, ProfessionalMatchRequestResponse, 
+                                ProfessionalResponseModel)
 from server.services import professional_service, resume_service, user_service, match_request_service, job_ad_service, company_service
-from server.services.professional_service import edit_professional_info, get_professional_info_by_id, edit_professional, get_professional_by_id
-from server.services.user_service import edit_user_info, get_town_id_by_name, valid_email
-
-
-class ProfessionalResponseModel(BaseModel):
-    professional: Professional
-    active_resumes: int
-
-
-class PersonalProfessionalResponseModel(BaseModel):
-    professional: Professional
-    active_resumes: int
-    list_of_matches: list[int]
+from server.services.professional_service import edit_professional, get_professional_by_id
+from server.services.user_service import edit_user_info
+from server.common.validations_and_methods import get_town_id_by_name, valid_email
 
 
 professionals_router = APIRouter(prefix='/professionals')
 
 
 @professionals_router.get('/')
-def get_professionals(search: str | None = None, search_by: str | None = None, sort: str | None = None, x_token=Header()):
+def get_professionals(search: str | None = None, search_by: str | None = None, sort: str | None = None, x_token=Header(None)):
     user = get_user_or_raise_401(x_token)
 
     search_validation = ['first_name', 'last_name', 'town_name', 'busy']
@@ -40,6 +30,7 @@ def get_professionals(search: str | None = None, search_by: str | None = None, s
 
     if sort and (sort == 'asc' or sort == 'desc'):
         return professional_service.sort(professionals, reverse=sort == 'desc')
+    
     else:
         if not professionals:
             return NotFound('Your search returned no results!')
@@ -48,43 +39,28 @@ def get_professionals(search: str | None = None, search_by: str | None = None, s
 
 
 @professionals_router.get('/{id}')
-def professional_by_id(id: int, x_token: str = Header()):
+def professional_by_id(id: int, x_token: str = Header(None)):
     user = get_user_or_raise_401(x_token)
+
+    if not user:
+        return Unauthorized('Please log in!')
 
     professional = get_professional_by_id(id)
 
     if professional is None:
         return NotFound('Professional not found!')
 
-    if user.id == id or user.is_admin():
-        return PersonalProfessionalResponseModel(
+    return ProfessionalResponseModel(
             professional=professional,
-            active_resumes=resume_service.get_number_of_all_active_resumes(id),
-            list_of_matches=resume_service.get_list_of_matches(id)
-        )
-
-    else:
-        return ProfessionalResponseModel(
-            professional=professional,
-            active_resumes=resume_service.get_number_of_all_active_resumes(id)
-        )
-
-
-@professionals_router.put('/{id}/info')
-def edit_professional_info_by_id(id: int, professional_info: ProfessionalInfo, x_token: str = Header()):
-    user = get_user_or_raise_401(x_token)
-
-    if not user.id == id or not user.is_admin():
-        return Unauthorized('You do not have permission to change Professional info!')
-
-    edited_professional_info = edit_professional_info(id, professional_info)
-
-    return edited_professional_info
+            active_resumes=resume_service.get_number_of_all_active_resumes(id))
 
 
 @professionals_router.put('/{id}')
-def edit_professional_by_id(id: int, professional: Professional, x_token: str = Header()):
+def edit_professional_by_id(id: int, professional: Professional, x_token: str = Header(None)):
     user = get_user_or_raise_401(x_token)
+
+    if not user:
+        return Unauthorized('Please log in!')
 
     if not user.id == id:
         return Forbidden('You do not have permission to change Professional info!')
@@ -95,25 +71,29 @@ def edit_professional_by_id(id: int, professional: Professional, x_token: str = 
     town_id = get_town_id_by_name(professional.town_name)
     if not town_id:
         return NotFound(f'Town {professional.town_name} is not a valid Bulgarian District Town name')
-    
-    error_msg = 'Your query is incorrect, please review your new info.'
 
     edited_user_info = edit_user_info(id, professional.email, professional.phone, professional.address, town_id)
+    
     if edited_user_info:
         edited_professional = edit_professional(id, professional)
+        if edited_professional:
+            return Success(f'Successfully updated info for professional with name {professional.first_name} {professional.last_name}!')
     else:
-        return BadRequest(error_msg)
-    
-    if edited_professional:
-        return Success(f'Successfully updated info for professional with name {professional.first_name} {professional.last_name}!')
-    else:
-        return BadRequest(error_msg)
+        return BadRequest('Your query is incorrect, please review your new info.')
+
 
 @professionals_router.get('/{id}/resumes')
-def professional_by_id(id: int, x_token: str = Header()):
+def professional_by_id(id: int, x_token: str = Header(None)):
     user = get_user_or_raise_401(x_token)
 
     if user:
+        if user.id == id:
+            resumes = resume_service.get_all_resumes_by_professional_id(id)
+            resumes_full = [ResumeWithSkillsResponseModel(full_name=user_service.get_professional_fullname_by_id(resume.professional_id), 
+                resume=resume, 
+                skills=resume_service.get_all_resume_skills_by_id(resume.id)) for resume in resumes]
+            return resumes_full
+
         if user.role == Role.COMPANY:
             resumes = resume_service.get_all_active_resumes_by_professional_id(id)
             resumes_full = [ResumeWithSkillsResponseModel(full_name=user_service.get_professional_fullname_by_id(resume.professional_id), 
@@ -121,17 +101,18 @@ def professional_by_id(id: int, x_token: str = Header()):
                 skills=resume_service.get_all_resume_skills_by_id(resume.id)) for resume in resumes]
             return resumes_full
         
-        elif user.role == Role.PROFESSIONAL:
+        if user.role == Role.PROFESSIONAL:
             resumes = resume_service.all_active_resumes_without_job_salary_and_description_by_professional_id(id)
             resumes_full = [ResumeWithoutDescriptionAndSalaryResponse(full_name=user_service.get_professional_fullname_by_id(resume.professional_id), 
                 resume=resume) for resume in resumes]
             return resumes_full
+            
     else:
-        return Forbidden('Please log in!')
+        return Unauthorized('Please log in!')
 
 
 @professionals_router.get('/{id}/match_requests')
-def get_match_requests(id: int, x_token: str = Header()):
+def get_match_requests(id: int, x_token: str = Header(None)):
     user = get_user_or_raise_401(x_token)
 
     if not user:
@@ -150,8 +131,9 @@ def get_match_requests(id: int, x_token: str = Header()):
                                             job_ad_service.get_job_ad_by_id(match_request.job_ad_id),
                                             resume_service.get_resume_by_id(match_request.resume_id)) for match_request in match_requests)
 
+
 @professionals_router.put('/{id}/match_requests/{match_request_id}')
-def match_job_ad(id: int, match_request_id: int, x_token: str = Header()):
+def match_job_ad(id: int, match_request_id: int, x_token: str = Header(None)):
     user = get_user_or_raise_401(x_token)
 
     if not user:
@@ -169,23 +151,20 @@ def match_job_ad(id: int, match_request_id: int, x_token: str = Header()):
 
     match_request_service.its_a_match(match_request_id)
     
-    #update professional to busy
     professional_service.make_professional_busy(id)
 
-    #update company's successful matches +1
     company_service.update_successful_matches(company.id)
 
-    #Resume status -> Matched
     resume_service.make_resume_matched(match_request.resume_id)
 
-    #JobAd status -> Archived
     job_ad_service.make_job_ad_archived(match_request.job_ad_id)
 
     #send email to company - its a match!
     return Success(f"You successfully matched {company.company_name}'s Job Ad!")
 
+
 @professionals_router.delete('/{id}/match_requests/{match_request_id}')
-def reject_match_request(id: int, match_request_id: int, x_token: str = Header()):
+def reject_match_request(id: int, match_request_id: int, x_token: str = Header(None)):
     user = get_user_or_raise_401(x_token)
 
     if not user:
